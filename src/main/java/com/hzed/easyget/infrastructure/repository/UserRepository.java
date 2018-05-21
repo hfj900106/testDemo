9a77185c70c2bc77122bb7df29299e41ae929132
wuchengwu <wuchengbwu@hzed.com>
2018-05-21 16:49:21 +0800

    验证码登录相关

--- src/main/java/com/hzed/easyget/infrastructure/repository/UserRepository.java
+++ src/main/java/com/hzed/easyget/infrastructure/repository/UserRepository.java
@@ -1,6 +1,13 @@
 package com.hzed.easyget.infrastructure.repository;
 
+import com.hzed.easyget.persistence.auto.entity.User;
+import com.hzed.easyget.persistence.auto.entity.example.UserExample;
+import com.hzed.easyget.persistence.auto.mapper.UserMapper;
+import com.hzed.service.common.util.ObjUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * demo
@@ -11,4 +18,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findByMobile(String mobile) {
+        UserExample example = new UserExample();
+        example.createCriteria().andMobileAccountEqualTo(mobile);
+        List<User> users = userMapper.selectByExample(example);
+        return ObjUtil.isEmpty(users) ? null : users.get(0);
+    }
+
 }

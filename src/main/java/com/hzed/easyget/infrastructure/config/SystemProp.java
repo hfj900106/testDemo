295cf4099de48be99aa737ceb15126be33f8f3a3
guichang <guichang@hzed.com>
2018-05-24 18:06:52 +0800

    增加过期时间配置

---
+++ src/main/java/com/hzed/easyget/infrastructure/config/SystemProp.java
@@ -0,0 +1,17 @@
+package com.hzed.easyget.infrastructure.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 系统参数配置
+ * @author guichang
+ */
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "system")
+public class SystemProp {
+    private String tokenExpire;
+}

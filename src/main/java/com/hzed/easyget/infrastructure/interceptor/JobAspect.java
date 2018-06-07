b360e44dc26d4f189bf72e606f66e4da54b2ccb8
hfj900106 <641903403@qq.com>
2018-06-07 20:48:12 +0800

    定时任务

--- src/main/java/com/hzed/easyget/infrastructure/interceptor/JobAspect.java
+++ src/main/java/com/hzed/easyget/infrastructure/interceptor/JobAspect.java
@@ -1,17 +1,92 @@
 package com.hzed.easyget.infrastructure.interceptor;
 
+import com.hzed.easyget.infrastructure.annotation.JobAnnotation;
+import com.hzed.easyget.infrastructure.config.JobProp;
+import com.hzed.easyget.infrastructure.utils.ComUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 /**
  * 定时任务统一拦截
- * @author hfj
- * @since 2018/6/7
+ *
+ * @author guichang
+ * @since 2018/2/5
  */
 @Slf4j
 @Aspect
 @Component
 public class JobAspect {
+
+    @Autowired
+    private JobProp jobProp;
+
+    /**
+     * 拦截定时任务主要处理类
+     */
+    @Around("@annotation(jobAnnotation)")
+    public Object aroundTest(ProceedingJoinPoint pPoint, JobAnnotation jobAnnotation) {
+        Object result = null;
+        String methodName = ((MethodSignature) pPoint.getSignature()).getMethod().getName();
+        // 如果配置不执行或定时任务正在进行直接返回
+        if (!getRunFlag(methodName)) {
+            return null;
+        }
+
+        // 设置运行标志-false
+        setRunFlag(methodName, false);
+        ComUtil.putTrace();// 放入日志追踪标志
+        // 定时任务名
+        //TODO
+//        MDCUtil.put("jobName", jobAnnotation.value());
+        log.info("=====定时任务 开始 =====");
+        try {
+            result = pPoint.proceed();
+        } catch (Throwable ex) {
+            ComUtil.expHandler(ex);
+        }
+        log.info("=====定时任务 结束 =====");
+        // 设置运行标志-true
+        setRunFlag(methodName, true);
+        return result;
+    }
+
+
+
+    /**
+     * 根据方法名获取运行标志
+     */
+    private boolean getRunFlag(String paraName) {
+        try {
+            Method a = JobProp.class.getMethod("is" + ComUtil.toUpperCaseIndex(paraName));
+            Boolean invoke = (Boolean) a.invoke(jobProp);
+            return invoke.booleanValue();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 设置运行标志
+     */
+    private void setRunFlag(String paraName, boolean flag) {
+        try {
+            for (Method method : JobProp.class.getMethods()) {
+                if (method.getName().equals("set" + ComUtil.toUpperCaseIndex(paraName))) {
+                    method.invoke(jobProp, flag);
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/com/hzed/easyget/infrastructure/utils/ComUtil.java
+++ src/main/java/com/hzed/easyget/infrastructure/utils/ComUtil.java
@@ -1,12 +1,92 @@
 package com.hzed.easyget.infrastructure.utils;
 
+
+import com.hzed.easyget.infrastructure.enums.BizCodeEnum;
+import com.hzed.easyget.infrastructure.exception.ComBizException;
+import com.hzed.easyget.infrastructure.exception.NestedException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+
 /**
- * @author guichang
- * @since 2018/3/19
+ * @author Administrator
  */
+@Slf4j
 public class ComUtil {
 
+    public static void checkComBizExcep(boolean condition, BizCodeEnum codeEnum) {
+        if (condition) {
+            throw new ComBizException(codeEnum);
+        }
+    }
 
+    private static final Random RANDOM = new Random();
 
+    public static void putTrace() {
+        //TODO
+//        MDCUtil.put(Consts.TRACE, Long.toHexString(RANDOM.nextLong()).toLowerCase());
+    }
+
+    /**
+     * 字符串首字母大写
+     */
+    public static String toUpperCaseIndex(String str) {
+        char[] cs = str.toCharArray();
+        cs[0] -= 32;
+        return String.valueOf(cs);
+
+    }
+
+    /**
+     * 产生随机码
+     */
+    public static String getRandomNumber(int size) {
+        //元素
+
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        //随机对象
+        Random rand = new Random();
+
+        //循环产生
+        for (int i = 10; i > 1; i--) {
+            int index = rand.nextInt(i);
+            int tmp = array[index];
+            array[index] = array[i - 1];
+            array[i - 1] = tmp;
+        }
+
+        //拼接结果为字符串
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            result = result * 10 + array[i];
+        }
+        String sixString = Integer.toString(result);
+
+        //有可能出现5位数，前面加0补全
+        if (sixString.length() == size - 1) {
+            sixString = "0" + sixString;
+        }
+        //打印结果
+        return sixString;
+    }
+
+    /**
+     * 通用处理异常方式
+     *
+     * @param ex 异常
+     */
+    public static void expHandler(Throwable ex) {
+        // 统一异常拦截处理
+        if (ex instanceof ComBizException) {
+            ComBizException cbEx = (ComBizException) ex;
+            log.error("异常，{}", cbEx.getSerializeMsg());
+        } else if (ex instanceof NestedException) {
+            NestedException nsEx = (NestedException) ex;
+            log.error("异常，{}", nsEx.getSerializeMsg(), nsEx);
+        } else {
+            log.error("异常", ex);
+        }
+    }
 
 }

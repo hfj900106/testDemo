0ed4ce852820a8013d66215d4d72b3e5be9a2032
Waylon <Waylon0923@163.com>
2018-06-11 20:04:46 +0800

    国际化初版开发完成

--- src/main/java/com/hzed/easyget/controller/web/I18nController.java
+++ src/main/java/com/hzed/easyget/controller/web/I18nController.java
@@ -1,8 +1,10 @@
 package com.hzed.easyget.controller.web;
 
 import com.hzed.easyget.application.service.LocaleMessageSourceService;
+import com.hzed.easyget.infrastructure.annotation.TokenIgnore;
 import com.hzed.easyget.infrastructure.enums.BizCodeEnum;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +15,15 @@ import javax.annotation.Resource;
  * @date 2018/6/8
  */
 @RestController
-@RequestMapping("/i18n/messages")
+@RequestMapping("/easy-get/i18n/messages")
 public class I18nController {
     @Resource
     private LocaleMessageSourceService localeMessageSourceService;
 
-    @GetMapping("/code")
-    public String test(String code) {
+    @TokenIgnore
+    @GetMapping("/codes/{code}")
+    public String test(@PathVariable("code") String code) {
+        System.out.println("code = " + code);
         String code1 = "bizcode." + BizCodeEnum.ILLEGAL_REQUEST.getCode();
         return localeMessageSourceService.getMessage(code1);
     }

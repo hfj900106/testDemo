e1cd7974432026d460a1aeca677d12be486fe6ee
zhangruilin <617401579@qq.com>
2018-06-15 11:37:03 +0800

    支付响应实体

---
+++ src/main/java/com/hzed/easyget/infrastructure/model/PayResponse.java
@@ -0,0 +1,16 @@
+package com.hzed.easyget.infrastructure.model;
+
+/**
+ * @author guichang
+ */
+import lombok.Data;
+
+@Data
+public class PayResponse {
+    private String code;
+
+    private String msg;
+
+    private boolean flag;
+
+}

e76073011ac303a90bdc251b635298eaeede91bc
guichang <guichang@hzed.com>
2018-06-22 15:13:16 +0800

    MQ优化

--- src/main/java/com/hzed/easyget/application/mq/MqConsumer.java
+++ src/main/java/com/hzed/easyget/application/mq/MqConsumer.java
@@ -1,13 +1,14 @@
 package com.hzed.easyget.application.mq;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hzed.easyget.application.enums.BluePayStatusEnum;
 import com.hzed.easyget.application.enums.TransactionTypeEnum;
 import com.hzed.easyget.application.service.RepayService;
 import com.hzed.easyget.application.service.TransactionService;
-import com.hzed.easyget.controller.model.BlueNotificationRequest;
+import com.hzed.easyget.controller.model.BluePayRequest;
 import com.hzed.easyget.infrastructure.consts.ComConsts;
-import com.hzed.easyget.infrastructure.enums.BluePayStatusEnum;
 import com.hzed.easyget.infrastructure.repository.TempTableRepository;
+import com.hzed.easyget.infrastructure.utils.MdcUtil;
 import com.hzed.easyget.infrastructure.utils.ValidatorUtil;
 import com.hzed.easyget.persistence.auto.entity.UserTransaction;
 import com.rabbitmq.client.Channel;
@@ -45,49 +46,59 @@ public class MqConsumer implements ChannelAwareMessageListener {
     private static final String BANK = "bank";
 
     @Override
-    public void onMessage(Message messageByte, Channel channel) throws Exception {
-        String message = new String(messageByte.getBody(), "UTF-8");
-        log.info("MQ回调消息：{}", message);
+    public void onMessage(Message messageByte, Channel channel) {
+        // 记录trace，方便日志追踪
+        MdcUtil.putTrace();
+        try {
+            String message = new String(messageByte.getBody(), "UTF-8");
+            log.info("放款/还款回调，bluepay端返回信息{}", message);
 
-        BlueNotificationRequest bluePayRq = JSONObject.parseObject(message, BlueNotificationRequest.class);
+            BluePayRequest bluePayRequest = JSONObject.parseObject(message, BluePayRequest.class);
 
-        ValidatorUtil.validate(bluePayRq);
-        log.info("放款/还款回调，bluepay端返回信息{}", JSONObject.toJSONString(bluePayRq));
-        //判断是否交易完成
-        Long tempId;
-        //过滤处理中
-        if (bluePayRq.getStatus().equals(BluePayStatusEnum.OK.getKey())) {
-            log.info("放款/还款bluepay端正在处理中！");
-            return;
-        }
-        //过滤失败直接修改交易记录
-        if (!bluePayRq.getStatus().equals(BluePayStatusEnum.BLUE_PAY_COMPLETE.getKey())) {
-            transactionService.updateUserTranState(bluePayRq.getT_id(), TransactionTypeEnum.FAIL_RANSACTION.getCode().byteValue());
-            log.info("放款/还款bluepay端返回处理失败{}", BluePayStatusEnum.getPayStatusEnum(bluePayRq.getStatus()));
-            return;
-        }
-        //获取交易id 判断是否合法
-        UserTransaction userTr = transactionService.findUserTranByPaymentId(bluePayRq.getT_id());
-        if (ObjectUtils.isEmpty(userTr)) {
-            return;
-        }
-        //判断这个交易是否是 交易中
-        if (userTr.getStatus().intValue() != TransactionTypeEnum.IN_RANSACTION.getCode()) {
-            return;
-        }
-        //放款
-        if (CASHOUT.equals(bluePayRq.getInterfacetype())) {
-            //查询相应的推送任务信息
-            tempId = tempTableRepository.findTempTableByBidNoAndName(userTr.getBidId(), ComConsts.PUSH_BANK_TASK);
-            //修改交易信息
-            transactionService.callBackupdateUserTrance(userTr, tempId);
-            log.info("放款bluepay端返回处理成功，本地处理成功");
-        }
-        //还款
-        if (BANK.equals(bluePayRq.getInterfacetype())) {
-            //TODO 走资金流
-            repayService.afterRepayment(userTr);
-            log.info("还款bluepay端返回处理成功，本地处理成功");
+            // 参数校验
+            ValidatorUtil.validateWithNull(bluePayRequest);
+
+            // 过滤处理中
+            if (bluePayRequest.getStatus().equals(BluePayStatusEnum.OK.getKey())) {
+                log.info("放款/还款bluepay端正在处理中");
+                return;
+            }
+
+            // 过滤失败直接修改交易记录
+            if (!bluePayRequest.getStatus().equals(BluePayStatusEnum.BLUE_PAY_COMPLETE.getKey())) {
+                transactionService.updateUserTranState(bluePayRequest.getT_id(), TransactionTypeEnum.FAIL_RANSACTION.getCode().byteValue());
+                log.info("放款/还款bluepay端返回处理失败{}", BluePayStatusEnum.getValueDesc(bluePayRequest.getStatus()));
+                return;
+            }
+
+            // 获取交易id 判断是否合法
+            UserTransaction userTr = transactionService.findUserTranByPaymentId(bluePayRequest.getT_id());
+            if (ObjectUtils.isEmpty(userTr)) {
+                return;
+            }
+
+            // 判断这个交易是否是 交易中
+            if (userTr.getStatus().intValue() != TransactionTypeEnum.IN_RANSACTION.getCode()) {
+                return;
+            }
+
+            // 放款
+            if (CASHOUT.equals(bluePayRequest.getInterfacetype())) {
+                // 查询相应的推送任务信息
+                Long tempId = tempTableRepository.findTempTableByBidNoAndName(userTr.getBidId(), ComConsts.PUSH_BANK_TASK);
+                // 修改交易信息
+                transactionService.callBackupdateUserTrance(userTr, tempId);
+                log.info("放款bluepay端返回处理成功，本地处理成功");
+            }
+
+            // 还款
+            if (BANK.equals(bluePayRequest.getInterfacetype())) {
+                // 走资金流
+                repayService.afterRepayment(userTr);
+                log.info("还款bluepay端返回处理成功，本地处理成功");
+            }
+        } catch (Exception ex) {
+            // TODO
         }
     }
 }
--- src/main/java/com/hzed/easyget/controller/model/BluePayRequest.java
+++ src/main/java/com/hzed/easyget/controller/model/BluePayRequest.java
@@ -13,32 +13,32 @@ import java.io.Serializable;
  * @date    2018年6月11日 上午9:12:05
  */
 @Data
-public class BlueNotificationRequest  implements Serializable {
-
+public class BluePayRequest implements Serializable {
     private static final long serialVersionUID = 3030045793860365162L;
-    @NotBlank(message = "cmd不能为空!")
+
+    @NotBlank(message = "cmd不能为空")
     private String cmd;
-    @NotBlank(message = "手机号码不能为空!")
+    @NotBlank(message = "手机号码不能为空")
     private String msisdn;
-    @NotBlank(message = "选择的银行不能为空!")
+    @NotBlank(message = "选择的银行不能为空")
     private String operator;
-    @NotBlank(message = "partner在调用接口时传来的transactionID不能为空!")
+    @NotBlank(message = "partner在调用接口时传来的transactionID不能为空")
     private String t_id;
-    @NotBlank(message = "BluePay在交易过程中生成的id不能为空!")
+    @NotBlank(message = "BluePay在交易过程中生成的id不能为空")
     private String bt_id;
-    @NotBlank(message = "交易状态不能为空!")
+    @NotBlank(message = "交易状态不能为空")
     private String status;
-    @NotNull(message = "交易金额不能为null!")
+    @NotNull(message = "交易金额不能为空")
     private Integer price;
     @Pattern(regexp = "^cashout|bank$", message = "值只能为cashout|bank")
     private String interfacetype;
-    @NotBlank(message = "用户手机号类型不能为空!")
+    @NotBlank(message = "用户手机号类型不能为空")
     private String paytype;
-    @NotBlank(message = "交易币种不能为空!")
+    @NotBlank(message = "交易币种不能为空")
     private String currency;
-    @NotNull(message = "产品id不能为null!")
+    @NotNull(message = "产品id不能为空")
     private Integer productid;
-    @NotBlank(message = "加密方式不能为空!")
+    @NotBlank(message = "加密方式不能为空")
     private String encrypt;
 
 

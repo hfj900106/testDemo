10d58c0a221576d11b848a929f82a26663b4b14c
hfj900106 <641903403@qq.com>
2018-06-29 12:40:16 +0800

    代码优化

---
+++ src/main/java/com/hzed/easyget/controller/model/FacebookInsRequest.java
@@ -0,0 +1,21 @@
+package com.hzed.easyget.controller.model;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * facebook和ins认证时数据推风控请求参数
+ * @author hfj
+ * @date 2018/6/18
+ */
+@Data
+public class FacebookInsRequest {
+    /**
+     * task_id
+     */
+    @NotBlank
+    private String task_id ;
+
+}

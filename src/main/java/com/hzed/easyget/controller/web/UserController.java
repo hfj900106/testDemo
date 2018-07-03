69446e334da8acbd1b2e061b297df9641ed4d67c
hfj900106 <641903403@qq.com>
2018-07-03 14:25:32 +0800

    交易记录翻页必填修改

--- src/main/java/com/hzed/easyget/controller/web/UserController.java
+++ src/main/java/com/hzed/easyget/controller/web/UserController.java
@@ -8,10 +8,13 @@ import com.hzed.easyget.infrastructure.annotation.ExceptionAnno;
 import com.hzed.easyget.infrastructure.annotation.ModuleFunc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * 我的
@@ -36,7 +39,7 @@ public class UserController {
 
     @ModuleFunc("交易记录")
     @PostMapping("/getTransactionRecord")
-    public TransactionRecordResponse getTransactionRecord(@RequestBody TransactionRecordRequest request) {
+    public TransactionRecordResponse getTransactionRecord(@Valid @RequestBody TransactionRecordRequest request) {
         return userService.getTransactionRecord(request);
     }
 

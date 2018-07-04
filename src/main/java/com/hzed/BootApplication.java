dd30063271c32a262f97e68c88bf00e7b8a17712
zhangruilin <617401579@qq.com>
2018-07-04 14:56:11 +0800

    接口超时处理优化

--- src/main/java/com/hzed/BootApplication.java
+++ src/main/java/com/hzed/BootApplication.java
@@ -137,21 +137,4 @@ public class BootApplication {
         return docket;
     }
 
-    /**
-     * rabbitmq相关配置
-     */
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(new Jackson2JsonMessageConverter());
-        return template;
-    }
-
-    @Bean(name = "rabbitListenerContainerFactory")
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        return factory;
-    }
 }

5300cd3c0fbe4406009f392e51a9bd3b5194cdd1
guichang <guichang@hzed.com>
2018-07-10 15:59:45 +0800

    代码优化

--- src/main/java/com/hzed/easyget/application/enums/AppVersionEnum.java
+++ src/main/java/com/hzed/easyget/application/enums/AppVersionEnum.java
@@ -8,15 +8,18 @@ import lombok.Getter;
 
 @Getter
 public enum AppVersionEnum {
-    /** 过规则 */
-    ANDROID("android","安卓"),
-    IOS("ios","苹果"),
-    ANDROID_VERSION("android_version","安卓版本号"),
-    IOS_VERSION("ios_update","苹果版本号"),
-    ANDROID_UPDATE("android_update","安卓版本号更新"),
-    IOS_UPDATE("ios_update","苹果版本号更新"),
-    NOT_UPDATE("1","不需要更新"),
-    HAS_UPDATE("0","需要更新");
+    /**
+     * 过规则
+     */
+    ANDROID("android", "安卓"),
+    IOS("ios", "苹果"),
+    ANDROID_VERSION("android_version", "安卓版本号"),
+    IOS_VERSION("ios_update", "苹果版本号"),
+    ANDROID_UPDATE("android_update", "安卓版本号更新"),
+    IOS_UPDATE("ios_update", "苹果版本号更新"),
+    NOT_UPDATE("1", "不需要更新"),
+    HAS_UPDATE("0", "需要更新");
+
     private String code;
     private String msg;
 

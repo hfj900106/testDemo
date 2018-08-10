76c75e92570156c08cb2c9f59a993836c1133473
hfj900106 <641903403@qq.com>
2018-08-10 10:29:09 +0800

    邮件限制长度、真实姓名限制长度及去掉多余空格

--- src/main/java/com/hzed/easyget/application/service/AuthService.java
+++ src/main/java/com/hzed/easyget/application/service/AuthService.java
@@ -280,6 +280,8 @@ public class AuthService {
         checkAuth(user.getUserId(), AuthCodeEnum.ID_CARD.getCode());
 
         String realName = request.getRealName();
+        // 姓名中多个空格替换成1个空格
+        realName = realName.replaceAll("\\s+", " ");
         String idCardNo = request.getIdCardNo();
         Integer gender = request.getGender();
         // 查询身份证是否已存在

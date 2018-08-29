fdd5f912caf07b0befd43d468cc87e3e44718c98
guichang <guichang@hzed.com>
2018-08-29 15:47:19 +0800

    mq回调加锁

--- src/main/java/com/hzed/easyget/application/mq/MqConsumer.java
+++ src/main/java/com/hzed/easyget/application/mq/MqConsumer.java
@@ -1,7 +1,10 @@
 package com.hzed.easyget.application.mq;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hzed.easyget.application.service.RepayService;
 import com.hzed.easyget.controller.model.BluePayRequest;
+import com.hzed.easyget.infrastructure.config.redis.RedisService;
+import com.hzed.easyget.infrastructure.consts.RedisConsts;
 import com.hzed.easyget.infrastructure.utils.MdcUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -16,11 +19,13 @@ import org.springframework.messaging.handler.annotation.Payload;
  * @date 2018年6月13日 下午6:10:40
  */
 @Slf4j
-@Profile({"test","prod"})
+@Profile({"test", "prod"})
 public class MqConsumer {
 
     @Autowired
     private RepayService repayService;
+    @Autowired
+    private RedisService redisService;
 
     @RabbitListener(queues = "BLUEPAY_ORDER_NOTIFICATION_QUEUE", containerFactory = "rabbitListenerContainerFactory")
     public void onMessage(@Payload BluePayRequest request) {
@@ -28,11 +33,18 @@ public class MqConsumer {
         // 记录trace，方便日志追踪
         MdcUtil.putTrace();
         log.info("============================= MQ交易回调开始 =============================");
+        // 防重拦截
+        String key = RedisConsts.BLUEPAY_MQ + RedisConsts.SPLIT + request.getT_id().trim() + RedisConsts.SPLIT + request.getStatus();
+        if (redisService.exists(key)) {
+            log.error("MQ回调消息重复，报文：{}", JSONObject.toJSON(request));
+            return;
+        }
         try {
             repayService.mqCallback(request);
         } catch (Exception ex) {
             log.error("============================= 自动处理失败，请执行人工处理程序 =============================", ex);
         } finally {
+            redisService.clearCache(key);
             log.info("============================= MQ交易回调结束 =============================");
         }
 

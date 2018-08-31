884e1e3bb8a6e41c7355026b8f0fee54796ffb45
hfj900106 <641903403@qq.com>
2018-08-31 17:10:52 +0800

    3期-个人信息认证重构

--- src/main/java/com/hzed/easyget/controller/model/PersonInfoAuthRequest.java
+++ src/main/java/com/hzed/easyget/controller/model/PersonInfoAuthRequest.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * 个人信息认证
@@ -47,5 +48,39 @@ public class PersonInfoAuthRequest {
     private String maritalStatus;
     @NotBlank(message = "{param.auth.birthMotherName.isNotEmpty}")
     private String birthMotherName;
+
+
+    /**
+     * 工作类型
+     */
+    @NotBlank(message = "{param.auth.jobType.isNotEmpty}")
+    private String jobType ;
+    /**
+     * 行业
+     */
+    @NotBlank(message = "{param.auth.industry.isNotEmpty}")
+    private String industry ;
+    /**
+     * 发薪日
+     */
+    @NotBlank(message = "{param.auth.payday.isNotEmpty}")
+    private String payday;
+    /**
+     * 月收入
+     */
+    @NotBlank(message = "{param.auth.monthlyIncome.isNotEmpty}")
+    private String monthlyIncome ;
+
+    /**
+     * 图片后缀
+     */
+    @NotBlank(message = "{param.auth.picSuffix.isNotEmpty}")
+    private String picSuffix;
+
+    /**
+     * 图片类型拼接base64字符串list
+     */
+    @NotNull(message = "{param.auth.picPathBase64Str.isNotEmpty}")
+    private List<String> picTypeAndPathBase64Str;
 
 }
--- src/main/java/com/hzed/easyget/infrastructure/repository/PersonInfoRepository.java
+++ src/main/java/com/hzed/easyget/infrastructure/repository/PersonInfoRepository.java
@@ -2,11 +2,17 @@ package com.hzed.easyget.infrastructure.repository;
 
 import com.hzed.easyget.persistence.auto.entity.Profile;
 import com.hzed.easyget.persistence.auto.entity.UserAuthStatus;
+import com.hzed.easyget.persistence.auto.entity.UserPic;
+import com.hzed.easyget.persistence.auto.entity.Work;
 import com.hzed.easyget.persistence.auto.mapper.ProfileMapper;
 import com.hzed.easyget.persistence.auto.mapper.UserAuthStatusMapper;
+import com.hzed.easyget.persistence.auto.mapper.UserPicMapper;
+import com.hzed.easyget.persistence.auto.mapper.WorkMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author hfj
@@ -17,11 +23,23 @@ public class PersonInfoRepository {
     @Autowired
     private ProfileMapper profileMapper;
     @Autowired
+    private WorkMapper workMapper;
+    @Autowired
     private UserAuthStatusMapper userAuthStatusMapper;
+    @Autowired
+    private UserPicMapper userPicMapper;
 
     @Transactional(rollbackFor = Exception.class)
-    public void insertPersonInfoAndUserAuthStatus(Profile profile, UserAuthStatus userAuthStatus) {
+    public void insertPersonInfoAndUserAuthStatus(Work work, Profile profile, List<UserAuthStatus> userAuthStatus,List<UserPic> list) {
+        userPicMapper.batchInsertSelective(list,UserPic.Column.id,UserPic.Column.userId,UserPic.Column.type,UserPic.Column.picUrl);
+        workMapper.insertSelective(work);
         profileMapper.insertSelective(profile);
-        userAuthStatusMapper.insertSelective(userAuthStatus);
+        userAuthStatusMapper.batchInsertSelective(userAuthStatus,
+                UserAuthStatus.Column.id,
+                UserAuthStatus.Column.userId,
+                UserAuthStatus.Column.authCode,
+                UserAuthStatus.Column.authStatus,
+                UserAuthStatus.Column.expireTime,
+                UserAuthStatus.Column.remark);
     }
 }

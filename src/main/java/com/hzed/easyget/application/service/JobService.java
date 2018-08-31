a77b5d9d722cb031a0b3568ef79f04ab94e2360f
guichang <guichang@hzed.com>
2018-08-31 17:41:10 +0800

    Merge remote-tracking branch 'remotes/origin/dev' into dev-sms
    # Conflicts:
    #	src/main/java/com/hzed/easyget/application/service/JobService.java
    #	src/test/java/MainTest.java

--- src/main/java/com/hzed/easyget/application/service/JobService.java
+++ src/main/java/com/hzed/easyget/application/service/JobService.java
@@ -30,6 +30,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Optional;
 

e32380ed0596593e551264cffb5625fb932d1136
hfj900106 <641903403@qq.com>
2018-09-06 17:00:47 +0800

    代码优化

--- src/main/java/com/hzed/easyget/application/service/AuthService.java
+++ src/main/java/com/hzed/easyget/application/service/AuthService.java
@@ -439,11 +439,17 @@ public class AuthService {
         // 查询身份证是否已存在
         User user = userRepository.findByIdCardNo(idCardNo);
         if (!ObjectUtils.isEmpty(user)) {
-            UserAuthStatus uasQuery = authStatusRepository.findEnableAuthStatusByUserId(userId, authCode);
-            // 身份认证成功不可重复认证
-            if (!ObjectUtils.isEmpty(uasQuery) && successAuthCode.equals(uasQuery.getAuthStatus())) {
+            // 比较已有记录的userId和当前userId是否相同，不同则直接拦截，相同则查认证数据是否是认证失败，否则拦截
+            if (!user.getId().equals(userId)) {
                 throw new WarnException(BizCodeEnum.IDCARD_EXIST);
+            } else {
+                UserAuthStatus uasQuery = authStatusRepository.findEnableAuthStatusByUserId(userId, authCode);
+                // 身份认证成功不可重复认证
+                if (!ObjectUtils.isEmpty(uasQuery) && successAuthCode.equals(uasQuery.getAuthStatus())) {
+                    throw new WarnException(BizCodeEnum.IDCARD_EXIST);
+                }
             }
+
         }
 
         // 调风控
